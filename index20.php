68b1b497db6ef7f6b761655b613bb2de5cc89143
patrikpatrik <axclassiceq@gmail.com>
2017-06-21 22:06:32 -0700

    TEST PHP

---
+++ index20.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<HTML>
+<HEAD>
+	<TITLE>Bouncy Ball mouse fling 2.0</TITLE>
+	<LINK REL="stylesheet" TYPE="text/css" HREF="./css/myrpg20.css"></LINK>
+</HEAD>
+<BODY>
+	<script type="application/javascript" src="./javascript/rectangle20.js"></script>
+	<p>Bouncy Ball mouse fling 2.0</p><p id="demo"></p><p id="loop"></p>
+	<button onclick="restart()">Reset!</button>
+</BODY>
+</HTML>
